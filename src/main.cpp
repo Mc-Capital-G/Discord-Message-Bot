a9545e5f4f1c70e5b09e673c9ba0576171301bdd
Declan <declanmcgrellis3@gmail.com>
2023-03-22 14:44:56 -0400

    file output setup

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 int main() {
 
-    
+    ofstream nounList;
+    nounList.open("../data/nouns.txt");
+    nounList << "write test";
+    nounList.close();
 
     return 0;
 }
